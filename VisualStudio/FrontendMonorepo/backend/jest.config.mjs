1d7e258e265830acfff495686666d2fead71cdb2
kuhnast01 <kuhnast01@gmail.com>
2026-02-22 21:46:18 -0500

    Updated jest.config.mjs

--- VisualStudio/FrontendMonorepo/backend/jest.config.mjs
+++ VisualStudio/FrontendMonorepo/backend/jest.config.mjs
@@ -3,21 +3,22 @@
  * Highest standards: ESM, TypeScript, monorepo, correct module resolution
  */
 export default {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
+  rootDir: './',
+  testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  rootDir: '.',
-  moduleDirectories: [
-    'node_modules',
-    '<rootDir>/node_modules',
-    '<rootDir>',
-  ],
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules', '<rootDir>'],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\.{1,2}/.*)\.js$': '$1',
     '^@models/(.*)$': '<rootDir>/../packages/models/src/$1',
     '^@api/(.*)$': '<rootDir>/../packages/api/src/$1',
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/../',
+    '<rootDir>/../../.vscode/',
+    '<rootDir>/../../extensions/',
+  ],
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
