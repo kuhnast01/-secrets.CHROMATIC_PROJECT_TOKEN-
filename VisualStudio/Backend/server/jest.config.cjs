e67074110652edc0af29276853c46753138b8d50
kuhnast01 <kuhnast01@gmail.com>
2026-02-22 21:11:19 -0500

    New jest.config.cjs

--- VisualStudio/Backend/server/jest.config.cjs
+++ VisualStudio/Backend/server/jest.config.cjs
@@ -1,16 +1,10 @@
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.test.js'],
-    // ...existing config...
-};
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    '**/__tests__/**/*.test.[jt]s',
-    '**/__tests__/**/*.test.[jt]sx',
-    '**/__tests__/**/*.spec.[jt]s',
-    '**/__tests__/**/*.spec.[jt]sx'
-  ],
-  // ...existing config...
-};
+  module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+      '**/__tests__/**/*.test.[jt]s',
+      '**/__tests__/**/*.test.[jt]sx',
+      '**/__tests__/**/*.spec.[jt]s',
+      '**/__tests__/**/*.spec.[jt]sx'
+    ],
+  };
