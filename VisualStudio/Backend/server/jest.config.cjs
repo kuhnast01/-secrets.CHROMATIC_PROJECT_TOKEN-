f0ed0e7eed646c86f2b877cf4c3abd1778cb1a65
kuhnast01 <kuhnast01@gmail.com>
2026-02-22 21:21:49 -0500

    jest.config.cjs Update

--- VisualStudio/Backend/server/jest.config.cjs
+++ VisualStudio/Backend/server/jest.config.cjs
@@ -1,10 +1,11 @@
-  module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: [
-      '**/__tests__/**/*.test.[jt]s',
-      '**/__tests__/**/*.test.[jt]sx',
-      '**/__tests__/**/*.spec.[jt]s',
-      '**/__tests__/**/*.spec.[jt]sx'
-    ],
-  };
+ module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s',
+    '**/__tests__/**/*.test.[jt]sx',
+    '**/__tests__/**/*.spec.[jt]s',
+    '**/__tests__/**/*.spec.[jt]sx'
+  ],
+}; 
+ 
